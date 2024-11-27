70e612e65a53ed8b2b723477abd0a2e001928903
Ira Bielova <118074852+irab228912@users.noreply.github.com>
2024-11-27 16:45:04 +0200

    Add files via upload

---
+++ Aiki_bot+keyboard++decorator/keyboard.py
@@ -0,0 +1,58 @@
+import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+import datetime
+from dotenv import load_dotenv
+import os
+
+# Функція для отримання токена з файлу .env
+def get_token():
+    load_dotenv()
+    return os.environ.get('API_TOKEN')
+
+
+def time_decorator(func):
+    def wrapper(*args, **kwargs):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"Час виклику функції: {current_time}")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+# Функція для створення кнопки "Повернутися назад↩️"
+def create_back_button():
+    btn_back = KeyboardButton('Повернутися назад↩️')
+    return btn_back
+
+# Функція для створення початкових кнопок
+def create_markup1lvl():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = KeyboardButton('Тайдзюцу (техніки тіла)')
+    btn2 = KeyboardButton('Букі ваза (техніки зі зброєю)')
+    markup.add(btn1, btn2)
+    return markup
+
+
+# Функція для створення нових кнопок (2 рівень)
+def create_markup2lvl():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    btn3 = KeyboardButton('Укемі (перекиди)')
+    btn4 = KeyboardButton('Кидки')
+    btn5 = KeyboardButton('Камаe (позиції)')
+    btn6 = KeyboardButton('Тай-сабакі (переміщення)')
+    btn7 = KeyboardButton('Дорі/торі (захвати)')
+    btn8 = KeyboardButton('Удари')
+    btn_back = create_back_button()
+    markup.add(btn3, btn4, btn5, btn6, btn7, btn8)
+    markup.add(btn_back)
+    return markup
+
+
+# Функція для створення нових кнопок (3 рівень)
+def create_markup3lvl():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    btn10 = KeyboardButton('Тачі ваза (стоячи)')
+    btn11 = KeyboardButton('Суварі ваза (сидячи)')
+    btn_back = create_back_button()
+    markup.add(btn10, btn11)
+    markup.add(btn_back)
+    return markup
